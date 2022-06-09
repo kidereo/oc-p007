e727228105507d04c04b7795c4a94ef384b9d07b
kidereo <kirill.derevenski@gmail.com>
2022-06-09 16:17:35 +0200

    Dropdown selectors change to reflect search in the main bar.

--- scripts/searchData.js
+++ scripts/searchData.js
@@ -6,16 +6,22 @@ async function reinit() {
     const input = document.getElementById("search-form-input");
     const query = input.value.toUpperCase();
     const recipeCardsSection = document.getElementById("recipe-cards");
+    const ulIngredients = document.getElementById("ingredient-list");
+    const ulAppliances = document.getElementById("appliance-list");
+    const ulUtensils = document.getElementById("utensil-list");
 
     /**
-     * Clear the cards section from all previous cards.
+     * Clear cards section and search selectors from all previous entries.
      *
      * @type {string}
      */
     recipeCardsSection.innerHTML = "";
+    ulIngredients.innerHTML = "";
+    ulAppliances.innerHTML = "";
+    ulUtensils.innerHTML = "";
 
     /**
-     * Send filtered data to index cards if input is 3 chars or more.
+     * Send filtered data to index cards if the input is 3 chars or more.
      */
     if (query.length >= 3) {
         displayData(recipes.filter(recipe =>
